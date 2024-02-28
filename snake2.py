10d0acaaa681215b46c7fbc404139ed6ad46b6f4
pabletefatimarocio <pabletefatima9@gmail.com>
2024-02-28 20:37:53 -0300

    head snake

--- snake2.py
+++ snake2.py
@@ -4,4 +4,18 @@ wn = turtle.Screen()
 wn.title("Game Snake")
 wn.bgcolor("black")
 wn.setup(width = 600, height = 600)
-wn.mainloop()
+wn.tracer(0)
+
+#wn.mainloop()
+
+
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0,0) 
+head.direction ="stop"
+
+while True: 
+    wn.update()
