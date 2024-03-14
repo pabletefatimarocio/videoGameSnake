5615623bd36f1ae8d541655fb283b97f6a8a05aa
pabletefatimarocio <pabletefatima9@gmail.com>
2024-03-13 22:44:05 -0300

    controles

--- snake2.py
+++ snake2.py
@@ -18,9 +18,19 @@ head.shape("square")
 head.color("white")
 head.penup()
 head.goto(0,0) 
-head.direction ="up"
+head.direction ="stop"
 
 #function
+def arriba():
+    head.direction = "up"
+def abajo():
+    head.direction = "down"
+def izquierda():
+    head.direction = "left"
+def derecha():
+    head.direction = "right"
+
+
 def mov():
     if head.direction == "up":
         y = head.ycor()
@@ -34,7 +44,14 @@ def mov():
     if head.direction == "right":
         x = head.xcor()
         head.setx( x + 20)
-    
+
+#Teclado
+wn.listen()
+wn.onkepress(arriba,"Up")
+wn.onkepress(abajo,"Down")
+wn.onkepress(izquierda,"Left")
+wn.onkepress(derecha,"Right")
+
 while True: 
     wn.update()
 
